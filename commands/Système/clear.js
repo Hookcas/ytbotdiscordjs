331e373680a2df977ada865aded5f6501893841d
Alexis <alexis.wenner2@orange.fr>
2019-07-20 17:25:30 +0200

    first commit

---
+++ commands/Système/clear.js
@@ -0,0 +1,30 @@
+const Command = require("../../modules/Command.js");
+
+class Clear extends Command {
+  constructor(client) {
+    super(client, {
+      name: "clear",
+      description: "Nettoyer un nombre de messages spécifiés.",
+      usage: "clear",
+      category: "Système",
+      permLevel: "Duelist"
+    });
+  }
+
+  async run(message, args) {
+    try {
+      await message.delete();
+      const msgToDelete = args[0]
+        ? `**${args[0]} messages supprimés.**`
+        : "Salon nettoyé (100 messages maximum par commande)";
+      message.channel.fetchMessages({ limit: args[0] }).then(messages => {
+        message.channel.bulkDelete(messages);
+        message.channel.send(msgToDelete).then(msg => msg.delete(5000));
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
+
+module.exports = Clear;
